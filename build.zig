cb24f3edf56eb549e9d22168b5ed38874be8c7e1
ekhadley <ekhadley@gmail.com>
2025-09-21 21:50:40 -0500

    stuff

--- build.zig
+++ build.zig
@@ -4,18 +4,24 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const lib_mod = b.createModule(.{
-        .root_source_file = b.path("src/root.zig"),
+        .root_source_file = b.path("src/rez.zig"),
         .target = target,
         .optimize = optimize,
     });
     const lib = b.addLibrary(.{
         .linkage = .dynamic,
-        .name = "zre",
+        .name = "rez",
         .root_module = lib_mod,
     });
     b.installArtifact(lib);
+    const tests_mod = b.createModule(.{
+        .root_source_file = b.path("src/tests.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    tests_mod.addImport("rez", lib_mod);
     const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
+        .root_module = tests_mod,
     });
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
     const test_step = b.step("test", "Run unit tests");
--- src/root.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-const testing = std.testing;
-
-pub const FilterState {
-    ???
-}
-
-test "basic add functionality" {}
